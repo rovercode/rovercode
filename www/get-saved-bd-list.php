3004cdb5943dc83ac2e876f5c66bf806445df4ed
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2015-12-25 18:04:50 -0500

    Adds step-through blockhighlighting. Clean-up.

--- www/get-saved-bd-list.php
+++ www/get-saved-bd-list.php
@@ -1,12 +1,23 @@
 <?php
+/**
+ * File Upload Handler
+ * 
+ * PHP version 5
+ *
+ * @category File_Io
+ * @package  Rovercode
+ * @author   Brady L. Hurlburt <bradyhurlburt@gmail.com>
+ * @license  http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GPL v2
+ * @link     https://github.com/aninternetof/rover-code
+ */
 
 $bds = glob('saved-bds/*.xml');
 
 $names = [];
 
-foreach($bds as $bd) {
-	$xml=simplexml_load_file($bd) or die("Error: Bad BD xml file.");
-	$names[] = (string)$xml->designName;
+foreach ($bds as $bd) {
+    $xml=simplexml_load_file($bd) or die("Error: Bad BD xml file.");
+    $names[] = (string)$xml->designName;
 }
 echo json_encode(array_values($names));
 
--- www/save-bd.php
+++ www/save-bd.php
@@ -1,14 +1,25 @@
 <?php
-	$filename = str_replace(' ', '_',$_POST["designName"]);
-	$filename = str_replace('.', '_',$filename);
-	$fd = fopen("saved-bds/$filename".".xml", "w");
-	$fileContents = array (
-		$_POST["designName"] => 'designName',
-		$_POST["bdString"] => 'bd',
-	);
-	$xml = new SimpleXMLElement('<root/>');
-	array_walk_recursive($fileContents, array($xml, 'addChild'));
-	fwrite($fd, $xml->asXML());
-	echo "Your design has been saved to the Rover";
-	fclose($fd);
+/**
+ * File Upload Handler
+ * 
+ * PHP version 5
+ *
+ * @category File_Io
+ * @package  Rovercode
+ * @author   Brady L. Hurlburt <bradyhurlburt@gmail.com>
+ * @license  http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GPL v2
+ * @link     https://github.com/aninternetof/rover-code
+ */
+$filename = str_replace(' ', '_', $_POST["designName"]);
+$filename = str_replace('.', '_', $filename);
+$fd = fopen("saved-bds/$filename".".xml", "w");
+$fileContents = array (
+    $_POST["designName"] => 'designName',
+    $_POST["bdString"] => 'bd',
+);
+$xml = new SimpleXMLElement('<root/>');
+array_walk_recursive($fileContents, array($xml, 'addChild'));
+fwrite($fd, $xml->asXML());
+echo "Your design has been saved to the Rover";
+fclose($fd);
 ?>
