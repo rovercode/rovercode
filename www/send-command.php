c4e6c7d5e5f30360cfb0170afad2ae78ad1dfbbc
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2016-05-09 18:27:46 +0000

    Renames send-motor-command to send-command

--- www/send-command.php
+++ www/send-command.php
@@ -4,7 +4,5 @@
   $objRedis->connect( "localhost" );
 
   /* Pass straight through to motor command queue */
-  $objRedis -> rPush('motorQueue', json_encode($_POST));
-
-  echo("made it through Redis php. success!");
+  $objRedis -> rPush('commandQueue', json_encode($_POST));
 ?>
