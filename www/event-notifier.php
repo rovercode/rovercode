fd96bd190504609ce5b6b7c39466ee2d227179e9
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2016-08-01 17:14:25 +0000

    Clears out events from before page is loaded. Fixes php time limit error

--- www/event-notifier.php
+++ www/event-notifier.php
@@ -2,9 +2,15 @@
 header('Content-Type: text/event-stream;charset=UTF-8');
 header('Cache-Control: no-cache');
 header("Connection: keep-alive");
+set_time_limit(0);
 
 $objRedis = new Redis();
 $objRedis->connect( "localhost" );
+
+$numStateEvents = $objRedis->llen('eventQueue');
+for ($i = 0; $i < $numStateEvents; $i++) {
+	$objRedis->lpop('eventQueue');
+}
 
 while(true) {
 	$event = $objRedis->lpop('eventQueue');
