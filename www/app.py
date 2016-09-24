dc0e7d535b229af0e430e63c752d8435a065f5fa
Taylor Brennan <taylor.brennan@inin.com>
2016-09-23 22:30:38 -0400

    [#26] remove GPIO stuff from app.py until we need it

--- www/app.py
+++ www/app.py
@@ -14,8 +14,6 @@ CORS(app)
 PWM_FREQ_HZ = 1000
 
 pwm = pwmLib.get_platform_pwm(pwmtype="softpwm")
-gpio = gpioLib.get_platform_gpio();
-
 
 @app.route('/api/v1/blockdiagrams', methods=['GET'])
 def getblockdiagrams():
@@ -48,56 +46,18 @@ def getblockdiagram(id):
 
 @app.route('/api/v1/sendcommand', methods = ['POST'])
 def sendCommand():
+    print request.form
     runCommand(request.json)
     return jsonify(request.json)
 
-class gpioPoller (threading.Thread):
-    	def __init__(self, threadID, name, pin, risingEvent, fallingEvent):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name = name
-		self.pin = pin
-		self.risingEvent = risingEvent
-		self.fallingEvent = fallingEvent
-
-	def run(self):
-		oldVal = True;
-		while (not killThreadsFlag):
-			newVal = gpio.is_high(self.pin)
-			if (oldVal == False) and (newVal == True):
-				print self.risingEvent
-				r.rpush('eventQueue', self.risingEvent)
-			elif (oldVal == True) and (newVal == False):
-				print self.fallingEvent
-				r.rpush('eventQueue', self.fallingEvent)
-			else:
-				pass
-			oldVal = newVal
-		self.name.exit()
-        
 def init_rover_service():
-    # Demonstrate overriding methods for debug if not on real hardware
-    if gpio.__class__.__name__ == 'DUMMYGPIOAdapter':
-	    def dummyGpioSetup(pin, mode, pull_up_down=gpioLib.PUD_OFF):
-		    print "Setting up dummy pin " + pin
-	    gpio.setup = dummyGpioSetup;
-
-    killThreadsFlag = False;
-
-    # set up IR sensor gpio
-    gpio.setup("XIO-P2", gpioLib.IN)
-    gpio.setup("XIO-P4", gpioLib.IN)
-    #rightEyePollingThread = gpioPoller(1, "rightEyePollingThread", "XIO-P4", 'rightEyeUncovered', 'rightEyeCovered')
-    #rightEyePollingThread.start();
-    #leftEyePollingThread = gpioPoller(2, "leftEyePollingThread", "XIO-P2", 'leftEyeUncovered', 'leftEyeCovered')
-    #leftEyePollingThread.start();
-
-    # set up motor gpio
+    # set up motor pwm
     pwm.start("XIO-P0", 0);
     pwm.start("XIO-P1", 0);
     pwm.start("XIO-P6", 0);
     pwm.start("XIO-P7", 0);
 
+    # test adapter
     if pwm.__class__.__name__ == 'DUMMY_PWM_Adapter':
         def mock_set_duty_cycle(pin, speed):
             print "Setting pin " + pin + " to speed " + str(speed)
