53cf1f38b0572e1bb2d1ec84fe78dde493a01cb8
wavetylor <wavetylor@gmail.com>
2016-09-26 22:05:25 -0400

    [#26] removing send-command.php, its been replaced

--- www/send-command.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  $objRedis = new Redis();
-  $objRedis->connect( "localhost" );
-
-  /* Pass straight through to motor command queue */
-  $objRedis -> rPush('commandQueue', json_encode($_POST));
-?>
