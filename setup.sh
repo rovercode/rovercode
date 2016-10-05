1e40e3720331bc20ecf833273cc5dd59af5486d6
Clifton Barnes <clifton.a.barnes@gmail.com>
2016-10-05 18:12:31 -0400

    Create a setup script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+ADAFRUIT_DIR=www/Adafruit_Python_GPIO
+
+if [ "${EUID}" -ne 0 ]; then
+	echo "Error: This script must be run as root."
+	exit 1
+fi
+
+if [ ! -e ${ADAFRUIT_DIR} ]; then
+	echo "Error: Adafruit directory not found."
+	exit 2
+fi
+
+apt-get install -y python python-dev python-pip python-smbus lighttpd
+pip install flask flask_cors
+
+pushd ${ADAFRUIT_DIR} > /dev/null
+python setup.py install
+popd > /dev/null
+
+echo "Setup complete."
+
+exit 0
