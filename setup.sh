3e607bd1fd61e21d2cf8a3dedf80ee04958d1f64
aninternetof <bradyhurlburt@gmail.com>
2016-10-19 14:14:26 -0400

    Make links to www and puts socket in /var/www

--- setup.sh
+++ setup.sh
@@ -12,16 +12,18 @@ if [ ! -e ${ADAFRUIT_DIR} ]; then
 	exit 2
 fi
 
-apt-get install -y python python-dev python-pip python-smbus nginx
-pip install flask uwsgi
+apt-get install -y python python-dev python-pip python-smbus nginx uwsgi
+pip install flask
 
 pushd ${ADAFRUIT_DIR} > /dev/null
 python setup.py install
 popd > /dev/null
 
-sed "s@__ROOTDIR__@$(pwd)/www@" nginx-site > rovercode-nginx-site
+rm -rf /var/www/rovercode > /dev/null
+mkdir /var/www/rovercode
+ln -s $(pwd)/www /var/www/rovercode/www
 rm -f /etc/nginx/sites-enabled/rovercode > /dev/null
-ln -s $(pwd)/rovercode-nginx-site /etc/nginx/sites-enabled/rovercode
+ln -s $(pwd)/nginx-site /etc/nginx/sites-enabled/rovercode
 service nginx restart
 
 echo "Setup complete."
