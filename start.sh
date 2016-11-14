bc6947402b1307a32c4a19de2d1661b6e72a19c6
aninternetof <bradyhurlburt@gmail.com>
2016-11-14 15:17:19 -0500

    Checks for existance of uwsgi in start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 INI_DIR=www/
+UWSGI_EXEC=uwsgi
 
 if [ "${EUID}" -ne 0 ]; then
 	echo "Error: This script must be run as root."
@@ -14,6 +15,10 @@ fi
 
 pushd ${INI_DIR} > /dev/null
 echo "rovercode service starting"
+if [ ! -e ${UWSGI_EXEC} ]; then
+	echo "Error: could not find uwsgi binary. Did you run start.sh?"
+	exit 2
+fi
 ./uwsgi rovercode.ini --plugins python,corerouter,http,gevent --gevent 1000 --http-websockets
 popd > /dev/null
 echo "rovercode service stopped"
