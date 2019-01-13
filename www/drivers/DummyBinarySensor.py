c7bae68669fa4743bac9bd7794b651b631f4ce25
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2019-01-13 10:34:00 -0500

    Put sensors thread in new ws loop

---
+++ www/drivers/DummyBinarySensor.py
@@ -0,0 +1,20 @@
+"""
+A dummy binary sensor that returns random values with no hardware dependency.
+"""
+
+import random
+import logging
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.getLevelName('INFO'))
+
+class DummyBinarySensor:
+    """A sensor that returns random values"""
+
+    def __init__(self):
+        LOGGER.info("Created dummy binary sensor")
+
+    def is_high(self):
+        """Return true or false randomly."""
+        return random.choice([True, False])
