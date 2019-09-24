164e8eebb82ade84bbc015f1603fa3e8102625ab
Brady L. Hurlburt <brady.hurlburt@genesys.com>
2019-09-24 04:03:30 -0400

    Add WiFi setup

--- commissioning/app.py
+++ commissioning/app.py
@@ -3,6 +3,10 @@ import argparse
 import glob
 import logging
 import os
+import subprocess
+import urllib.request
+import urllib.error
+from time import sleep
 from dotenv import dotenv_values
 
 logging.basicConfig()
@@ -10,20 +14,18 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.getLevelName('INFO'))
 
 
-def update_env_file(env_source_directory, env_dest_directory):
+def connected_to_internet():
+    """Check if we haven an Internet connection."""
+    try:
+        urllib.request.urlopen('https://google.com')
+        return True
+    except urllib.error.URLError:
+        return False
+
+
+def update_env_file(source_values, env_dest_directory):
     """Copy configuration information from one file to another."""
     env_dest_path = env_dest_directory + '.env'
-
-    source_files = list(filter(
-        os.path.isfile,
-        glob.glob(env_source_directory + "*.env")))
-    if not source_files:
-        LOGGER.warning(f'No .env files found at {env_source_directory}')
-        return
-    source_files.sort(key=lambda x: os.path.getmtime(x))
-    source_file = source_files[-1]
-    source_values = dotenv_values(dotenv_path=source_file)
-
     if not os.path.exists(env_dest_directory):
         os.makedirs(env_dest_directory)
     if not os.path.exists(env_dest_path):
@@ -37,6 +39,31 @@ def update_env_file(env_source_directory, env_dest_directory):
     LOGGER.info("Done updating .env file.")
 
 
+def configure_wpa_supplicant(ssid, psk):
+    """Configure wpa_supplicant."""
+    LOGGER.info('Configuring WiFi.')
+    result = subprocess.run(['wpa_cli add_network'], capture_output=True)
+    network_id = result.stdout[-1]
+    commands = [
+        [f'wpa_cli set_network {network_id} ssid \'"{ssid}"\''],
+        [f'wpa_cli set_network {network_id} psk \'"{psk}"\''],
+        [f'wpa_cli enable_network {network_id}'],
+        ['wpa_cli save_config'],
+        ['wpa_cli -i wlan0 reconfigure'],
+    ]
+    for command in commands:
+        sleep(0.2)
+        subprocess.run(command)
+    timeout = 0
+    while(not connected_to_internet() and timeout < 10):
+        timeout += 1
+        sleep(0.5)
+    if timeout == 10:
+        LOGGER.error('WiFi never came up.')
+        return
+    LOGGER.info('Done configuring WiFi.')
+
+
 def main():
     """Kick off all the commissioning work."""
     parser = argparse.ArgumentParser()
@@ -44,9 +71,24 @@ def main():
     parser.add_argument("destination", help="destination file directory")
     args = parser.parse_args()
 
+    source_files = list(filter(
+        os.path.isfile,
+        glob.glob(args.source + "*.env")))
+    print(source_files)
+    if not source_files:
+        LOGGER.warning(f'No .env files found at {args.source}')
+        return
+    source_files.sort(key=lambda x: os.path.getmtime(x))
+    source_file = source_files[-1]
+    source_values = dotenv_values(dotenv_path=source_file)
+
     LOGGER.info("Beginning commissioning.")
-    update_env_file(args.source, args.destination)
-    # TODO: update wfa_supplicant.conf
+    if source_values:
+        ssid = source_values.pop('AP_NAME')
+        psk = source_values.pop('AP_PASSWORD')
+        update_env_file(source_values, args.destination)
+        if ssid and psk and not connected_to_internet():
+            configure_wpa_supplicant(ssid, psk)
     LOGGER.info("Finished commissioning.")
 
 
--- commissioning/tests/test_app.py
+++ commissioning/tests/test_app.py
@@ -1,5 +1,7 @@
 """Test the app module."""
 import argparse
+import subprocess
+import urllib.error
 from unittest.mock import patch, mock_open, call
 
 import app
@@ -8,7 +10,8 @@ import app
 def get_env_data(dotenv_path):
     """Return mock config data."""
     if dotenv_path == 'source.env':
-        return {'a': '1', 'b': '2'}
+        return {'a': '1', 'b': '2',
+                'AP_NAME': 'foo', 'AP_PASSWORD': 'bar'}
     if dotenv_path == 'dest/.env':
         return {'b': '200', 'c': 'asdf'}
     return None
@@ -17,13 +20,11 @@ def get_env_data(dotenv_path):
 @patch('builtins.open', new_callable=mock_open)
 @patch('app.dotenv_values')
 @patch('os.path')
-@patch('glob.glob')
-def test_updating_env_file(mock_glob, mock_path, mock_dotenv, mock_dest_open):
+def test_updating_env_file(mock_path, mock_dotenv, mock_dest_open):
     """Test updating an env file."""
-    mock_glob.return_value = ['source.env']
     mock_path.exists.return_value = True
     mock_dotenv.side_effect = get_env_data
-    app.update_env_file('src/', 'dest/')
+    app.update_env_file(get_env_data('source.env'), 'dest/')
     mock_dest_open.assert_called_with('dest/.env', 'w')
     mock_dest_handle = mock_dest_open()
     mock_dest_handle.write.assert_has_calls(
@@ -31,27 +32,16 @@ def test_updating_env_file(mock_glob, mock_path, mock_dotenv, mock_dest_open):
         any_order=True)
 
 
-@patch('builtins.open', new_callable=mock_open)
-@patch('glob.glob')
-def test_updating_env_file_no_files(mock_glob, mock_dest_open):
-    """Test updating an env file but no source files found."""
-    mock_glob.return_value = []
-    app.update_env_file('src/', 'dest/')
-    mock_dest_open.assert_not_called()
-
-
 @patch('os.makedirs')
 @patch('builtins.open', new_callable=mock_open)
 @patch('app.dotenv_values')
 @patch('os.path')
-@patch('glob.glob')
-def test_updating_env_file_no_dest_path(mock_glob, mock_path, mock_dotenv,
+def test_updating_env_file_no_dest_path(mock_path, mock_dotenv,
                                         mock_dest_open, mock_makedirs):
     """Test updating an env file."""
-    mock_glob.return_value = ['source.env']
     mock_path.exists.return_value = False
     mock_dotenv.side_effect = get_env_data
-    app.update_env_file('src/', 'dest/')
+    app.update_env_file(get_env_data('source.env'), 'dest/')
     mock_makedirs.assert_called_with('dest/')
     mock_dest_open.assert_called_with('dest/.env', 'w')
     mock_dest_handle = mock_dest_open()
@@ -60,10 +50,92 @@ def test_updating_env_file_no_dest_path(mock_glob, mock_path, mock_dotenv,
         any_order=True)
 
 
+@patch('app.dotenv_values')
 @patch('argparse.ArgumentParser.parse_args',
        return_value=argparse.Namespace(source='src/', destination='dest/'))
+@patch('app.configure_wpa_supplicant')
+@patch('app.connected_to_internet')
 @patch('app.update_env_file')
-def test_main(mock_app, _):
+@patch('os.path.getmtime')
+@patch('os.path.isfile')
+@patch('glob.glob')
+def test_main(mock_glob, mock_isfile, mock_mtime, mock_update_env,
+              mock_is_connected, mock_configure_wpa, _, mock_dotenv):
     """Test main function."""
+    mock_isfile.return_value = True
+    mock_glob.return_value = ['source.env']
+    mock_dotenv.side_effect = get_env_data
+    mock_is_connected.return_value = False
     app.main()
-    mock_app.assert_called_with('src/', 'dest/')
+    mock_glob.assert_called_with('src/*.env')
+    mock_mtime.assert_called_with('source.env')
+    expected = get_env_data('source.env').copy()
+    ap_name = expected.pop('AP_NAME')
+    ap_password = expected.pop('AP_PASSWORD')
+    mock_update_env.assert_called_with(expected, 'dest/')
+    mock_configure_wpa.assert_called_with(ap_name, ap_password)
+
+
+@patch('argparse.ArgumentParser.parse_args',
+       return_value=argparse.Namespace(source='src/', destination='dest/'))
+@patch('app.configure_wpa_supplicant')
+@patch('app.connected_to_internet')
+@patch('app.update_env_file')
+@patch('os.path.isfile')
+@patch('glob.glob')
+def test_main_no_source_files(mock_glob, mock_isfile, mock_update_env,
+                              mock_is_connected, mock_configure_wpa, _):
+    """Test main function."""
+    mock_is_connected.return_value = True
+    mock_isfile.return_value = True
+    mock_glob.return_value = []
+    app.main()
+    mock_glob.assert_called_with('src/*.env')
+    mock_update_env.assert_not_called()
+    mock_configure_wpa.assert_not_called()
+
+
+@patch('urllib.request')
+def test_connected_to_internet(mock_url_request):
+    """Test successful connection to the Internet."""
+    assert app.connected_to_internet()
+    mock_url_request.urlopen.assert_called_with('https://google.com')
+
+
+@patch('urllib.request')
+def test_connected_to_internet_failed(mock_url_request):
+    """Test when cannot connect to the Internet."""
+    mock_url_request.urlopen.side_effect = urllib.error.URLError("No internet")
+    assert not app.connected_to_internet()
+
+
+WPA_CALLS = [
+    call(['wpa_cli add_network'], capture_output=True),
+    call(['wpa_cli set_network 5 ssid \'"test_ssid"\'']),
+    call(['wpa_cli set_network 5 psk \'"test_psk"\'']),
+    call(['wpa_cli enable_network 5']),
+    call(['wpa_cli save_config']),
+    call(['wpa_cli -i wlan0 reconfigure'])
+]
+
+
+@patch('app.connected_to_internet')
+@patch('subprocess.run')
+def test_configure_wifi(mock_subprocess, mock_connected):
+    """Test configuring the WiFi."""
+    mock_connected.return_value = True
+    mock_subprocess.return_value = subprocess.CompletedProcess(
+        [], 0, stdout='Network ok\n 5')
+    app.configure_wpa_supplicant('test_ssid', 'test_psk')
+    mock_subprocess.assert_has_calls(WPA_CALLS)
+
+
+@patch('app.connected_to_internet')
+@patch('subprocess.run')
+def test_configure_wifi_doesnt_come_up(mock_subprocess, mock_connected):
+    """Test when the WiFi interface doesn't come up."""
+    mock_connected.return_value = False
+    mock_subprocess.return_value = subprocess.CompletedProcess(
+        [], 0, stdout='Network ok\n 5')
+    app.configure_wpa_supplicant('test_ssid', 'test_psk')
+    mock_subprocess.assert_has_calls(WPA_CALLS)
